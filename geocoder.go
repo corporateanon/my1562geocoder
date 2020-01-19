53241f67be121b51fc833c9a077dd289c526f3a8
no <no>
2020-01-19 21:40:28 +0200

    wip

--- geocoder.go
+++ geocoder.go
@@ -1,9 +1,63 @@
 package my1562geocoder
 
+import (
+	"math"
+)
+
 func GetAddressByID(id uint32) *Address {
 	addr, ok := Addresses[id]
-	if(ok) {
+	if ok {
 		return &addr
 	}
 	return nil
+}
+
+const meridianDegreeLength = 111000.0
+
+func parallelDegreeLength(lat float64) float64 {
+	return math.Cos(lat*math.Pi/180.0) * meridianDegreeLength
+}
+
+func transpose(lat float64, lng float64, dLatMeters float64, dLngMeters float64) (float64, float64) {
+	dLatDegrees := dLatMeters / meridianDegreeLength
+	dLngDegrees := dLngMeters / parallelDegreeLength(lat)
+	return lat + dLatDegrees, lng + dLngDegrees
+}
+
+func distance(lat0, lng0, lat1, lng1 float64) float64 {
+	dLatMeters := (lat0 - lat1) * meridianDegreeLength
+	dLngMeters := (lng0 - lng1) * parallelDegreeLength(lat0)
+	return math.Sqrt(dLatMeters*dLatMeters + dLngMeters*dLngMeters)
+}
+
+type Rectangle struct {
+	iLat int32
+	iLng int32
+}
+
+func getRectangleByLatLng(lat float64, lng float64, res int32) Rectangle {
+	var iLat, iLng int32
+	iLat = int32((lat + 90.0) * GeoIndexResolution)
+	iLng = int32((lng + 180.0) * GeoIndexResolution)
+	return Rectangle{iLat, iLng}
+}
+
+func getNearbyRectangles(lat float64, lng float64, res int32, accuracyMeters float64) []Rectangle {
+	minLat, minLng := transpose(lat, lng, -accuracyMeters, -accuracyMeters)
+	maxLat, maxLng := transpose(lat, lng, accuracyMeters, accuracyMeters)
+
+	minRect := getRectangleByLatLng(minLat, minLng, res)
+	maxRect := getRectangleByLatLng(maxLat, maxLng, res)
+
+	length := (maxRect.iLat - minRect.iLat + 1) * (maxRect.iLng - minRect.iLng + 1)
+	slice := make([]Rectangle, length)
+	index := 0
+	for iLat := minRect.iLat; iLat <= maxRect.iLat; iLat++ {
+		for iLng := minRect.iLng; iLng <= maxRect.iLng; iLng++ {
+			slice[index] = Rectangle{iLat, iLng}
+			index++
+		}
+	}
+
+	return slice
 }
--- geocoder_test.go
+++ geocoder_test.go
@@ -1,6 +1,7 @@
 package my1562geocoder
 
 import (
+	"math"
 	"reflect"
 	"testing"
 )
@@ -18,16 +19,16 @@ func TestGetAddressById(t *testing.T) {
 			"Existing address",
 			args{181901},
 			&Address{
-				ID: 181901,
-				Lat: 49.9604596373,
-				Lng: 36.3265315275,
-				Number: 4,
-				Suffix: "",
-				Block: "",
-				StreetID: 4453,
-				Detail: "",
+				ID:           181901,
+				Lat:          49.9604596373,
+				Lng:          36.3265315275,
+				Number:       4,
+				Suffix:       "",
+				Block:        "",
+				StreetID:     4453,
+				Detail:       "",
 				DetailNumber: 0,
-				Postcode: 61082,
+				Postcode:     61082,
 			},
 		},
 		{
@@ -40,6 +41,92 @@ func TestGetAddressById(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := GetAddressByID(tt.args.id); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetAddressById() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getRectangleByLatLng(t *testing.T) {
+	type args struct {
+		lat float64
+		lng float64
+		res int32
+	}
+	tests := []struct {
+		name string
+		args args
+		want Rectangle
+	}{
+		{
+			"Resolve coordinates to indexing rectangle",
+			args{49.944204004899994, 36.3421038691, 200},
+			Rectangle{27988, 43268},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getRectangleByLatLng(tt.args.lat, tt.args.lng, tt.args.res); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getRectangleByLatLng() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getNearbyRectangles(t *testing.T) {
+	type args struct {
+		lat            float64
+		lng            float64
+		res            int32
+		accuracyMeters float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want []Rectangle
+	}{
+		{
+			"Get slice of nearby rectangles for a point",
+			args{49.944204004899994, 36.3421038691, 200, 100},
+			[]Rectangle{
+				{27988, 43268},
+				{27989, 43268},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getNearbyRectangles(tt.args.lat, tt.args.lng, tt.args.res, tt.args.accuracyMeters); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getNearbyRectangles() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_distance(t *testing.T) {
+	type args struct {
+		lat0 float64
+		lng0 float64
+		lat1 float64
+		lng1 float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{
+			"Measures distance",
+			args{
+				50.00577382744244, 36.22907459735871,
+				49.92835056065926, 36.308602094650276,
+			},
+			10300,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := math.Round(distance(tt.args.lat0, tt.args.lng0, tt.args.lat1, tt.args.lng1)/100) * 100; got != tt.want {
+				t.Errorf("distance() = %v, want %v", got, tt.want)
 			}
 		})
 	}
